3941857d229b080eb9c68e20c6c90d2250ebadf1
AntonioLucio08 <jalg0822@gmail.com>
2022-04-28 16:39:08 -0500

    nueva carpeta

--- ejercicios.js
+++ ejercicios.js
@@ -1,74 +1,74 @@
 const boolenaos = false;
 
-const myLista= [1,2,3,4,5];
+const myLista = [1, 2, 3, 4, 5];
 
 const string = 'notas';
 
-const numeros= 12345;
+const numeros = 12345;
 
 const persona = {
-     apellido: 'Lucio',
-     nombre:'Antonio',
-     edad: 27,
-     estatura: 1.77
- }
-
- console.log(numeros);
- console.log(boolenaos);
- console.log(string);
- console.log(persona);
-
- function suma (x , y){
-
-     let resultado = x + y;
-     return resultado;
-
-    
- }
-console.log(suma(5,5))
-
-function resta (x,y){
-
-    let resultado = x-y;
-    return resultado;
-}
-console.log(resta(50,35));
-
-function divicion(x,y){
-
-    let resultado = x/y;
-    return resultado;
-}
-console.log( divicion(20,5));
-
-function multiplicacion (x,y){
-
-    let resultado = x*y;
-    return resultado;
+    apellido: 'Lucio',
+    nombre: 'Antonio',
+    edad: 27,
+    estatura: 1.77
 }
 
-console.log(multiplicacion(3,3));
+console.log(numeros);
+console.log(boolenaos);
+console.log(string);
+console.log(persona);
 
-function operaciones (x,y,tipoOperacion){
+function suma(x, y) {
+
+    let resultado = x + y;
+    return resultado;
+
+
+}
+console.log(suma(5, 5))
+
+function resta(x, y) {
+
+    let resultado = x - y;
+    return resultado;
+}
+console.log(resta(50, 35));
+
+function divicion(x, y) {
+
+    let resultado = x / y;
+    return resultado;
+}
+console.log(divicion(20, 5));
+
+function multiplicacion(x, y) {
+
+    let resultado = x * y;
+    return resultado;
+}
+
+console.log(multiplicacion(3, 3));
+
+function operaciones(x, y, tipoOperacion) {
     let resultado;
 
-    if (tipoOperacion == 'suma'){
-    resultado = x + y;
-    return resultado
-}else if (tipoOperacion == 'resta'){
-    resultado = x-y;
-    return resultado
-}else if (tipoOperacion=='divicion'){
-    resultado = x/y;
-    return resultado
-}else if(tipoOperacion == 'multiplicacion'){
-    resultado= x+y;
-    return resultado
-}
+    if (tipoOperacion == 'suma') {
+        resultado = x + y;
+        return resultado
+    } else if (tipoOperacion == 'resta') {
+        resultado = x - y;
+        return resultado
+    } else if (tipoOperacion == 'divicion') {
+        resultado = x / y;
+        return resultado
+    } else if (tipoOperacion == 'multiplicacion') {
+        resultado = x + y;
+        return resultado
+    }
 
 
 }
-console.log(operaciones(1,2,'-'))
+console.log(operaciones(1, 2, '-'))
 
 
 
@@ -78,15 +78,15 @@ const aritmetica = (num1, num2, tipoOperacion) => {
 
     let resultado;
 
-    if(tipoOperacion == 'suma'){
+    if (tipoOperacion == 'suma') {
         return resultado = num1 + num2
-    } else if (tipoOperacion == 'resta'){
+    } else if (tipoOperacion == 'resta') {
         return resultado = num1 - num2
-    } else if (tipoOperacion == 'division'){
+    } else if (tipoOperacion == 'division') {
         return resultado = num1 / num2
     } else {
-        return resultado = num1*num2
+        return resultado = num1 * num2
     }
 }
 
-console.log(aritmetica(3,5,'resta'))
+console.log(aritmetica(3, 5, 'resta'))
